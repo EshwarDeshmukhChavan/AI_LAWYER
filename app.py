3060a082e60cf0e922571e57052060fc6fb0d310
Eshwar Deshmukh Chavan <117294108+EshwarDeshmukhChavan@users.noreply.github.com>
2025-08-05 23:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import hashlib
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.Youtubeing import load_qa_chain
+
 from langchain.prompts import PromptTemplate
 from langchain.docstore.document import Document
 
